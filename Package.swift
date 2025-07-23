488e85461598e09416eaf1606ceb5cbfb9e377ea
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-07-23 22:22:34 +0200

    Update package dependencies and remove web framework coupling
    Remove coenttb-web dependencies and replace with focused server dependencies.
    Add AsyncHTTPClient, swift-dependencies, and other core server packages.
    Simplify dependency graph for better maintainability.

--- Package.swift
+++ Package.swift
@@ -1,16 +1,12 @@
 // swift-tools-version:6.0
 
-import Foundation
 import PackageDescription
 
 extension String {
     static let coenttbServer: Self = "Coenttb Server"
     static let coenttbServerRouter: Self = "Coenttb Server Router"
     static let coenttbServerEnvVars: Self = "Coenttb Server EnvVars"
-    static let coenttbServerHTML: Self = "Coenttb Server HTML"
     static let coenttbServerDependencies: Self = "Coenttb Server Dependencies"
-    static let coenttbServerModels: Self = "Coenttb Server Models"
-    static let coenttbServerTranslations: Self = "Coenttb Server Translations"
     static let coenttbServerUtils: Self = "Coenttb Server Utils"
     static let coenttbDatabase: Self = "Coenttb Database"
 }
@@ -18,31 +14,25 @@ extension String {
 extension Target.Dependency {
     static var coenttbServer: Self { .target(name: .coenttbServer) }
     static var coenttbServerEnvVars: Self { .target(name: .coenttbServerEnvVars) }
-    static var coenttbServerHTML: Self { .target(name: .coenttbServerHTML) }
     static var coenttbServerDependencies: Self { .target(name: .coenttbServerDependencies) }
-    static var coenttbServerModels: Self { .target(name: .coenttbServerModels) }
-    static var coenttbServerTranslations: Self { .target(name: .coenttbServerTranslations) }
     static var coenttbDatabase: Self { .target(name: .coenttbDatabase) }
     static var coenttbServerUtils: Self { .target(name: .coenttbServerUtils) }
     static var coenttbServerRouter: Self { .target(name: .coenttbServerRouter) }
 }
 
 extension Target.Dependency {
-    static var coenttbWeb: Self { .product(name: "Coenttb Web", package: "coenttb-web") }
-    static var coenttbWebEnvVars: Self { .product(name: "Coenttb Web EnvVars", package: "coenttb-web") }
-    static var coenttbWebHTML: Self { .product(name: "Coenttb Web HTML", package: "coenttb-web") }
-    static var coenttbWebDependencies: Self { .product(name: "Coenttb Web Dependencies", package: "coenttb-web") }
-    static var coenttbWebModels: Self { .product(name: "Coenttb Web Models", package: "coenttb-web") }
+    static var asyncHttpClient: Self { .product(name: "AsyncHTTPClient", package: "async-http-client") }
     static var coenttbWebTranslations: Self { .product(name: "Coenttb Web Translations", package: "coenttb-web") }
-    static var coenttbWebLegal: Self { .product(name: "Coenttb Web Legal", package: "coenttb-web") }
-    static var coenttbWebUtils: Self { .product(name: "Coenttb Web Utils", package: "coenttb-web") }
     static var casePaths: Self { .product(name: "CasePaths", package: "swift-case-paths") }
-    static var fluent: Self { .product(name: "Fluent", package: "fluent") }
+    static var dependencies: Self { .product(name: "Dependencies", package: "swift-dependencies") }
+    static var dependenciesTestSupport: Self { .product(name: "DependenciesTestSupport", package: "swift-dependencies") }
+    static var environmentVariables: Self { .product(name: "EnvironmentVariables", package: "swift-environment-variables") }
+    static var languages: Self { .product(name: "Languages", package: "swift-language") }
+    static var logging: Self { .product(name: "Logging", package: "swift-log") }
     static var rateLimiter: Self { .product(name: "RateLimiter", package: "coenttb-utils") }
     static var postgresKit: Self { .product(name: "PostgresKit", package: "postgres-kit") }
-    static var fluentPostgresDriver: Self { .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver") }
     static var issueReporting: Self { .product(name: "IssueReporting", package: "xctest-dynamic-overlay") }
-    static var pointfreeServer: Self { .product(name: "PointFree Server", package: "pointfree-server") }
+    static var urlRouting: Self { .product(name: "URLRouting", package: "swift-url-routing") }
 }
 
 let package = Package(
@@ -57,42 +47,36 @@ let package = Package(
             targets: [
                 .coenttbServer,
                 .coenttbServerEnvVars,
-                .coenttbServerHTML,
                 .coenttbServerDependencies,
-                .coenttbServerModels,
-                .coenttbServerTranslations,
                 .coenttbDatabase,
                 .coenttbServerUtils,
-                .coenttbServerRouter,
+                .coenttbServerRouter
             ]
         ),
         .library(name: .coenttbServerEnvVars, targets: [.coenttbServerEnvVars]),
-        .library(name: .coenttbServerHTML, targets: [.coenttbServerHTML]),
         .library(name: .coenttbServerDependencies, targets: [.coenttbServerDependencies]),
-        .library(name: .coenttbServerModels, targets: [.coenttbServerModels]),
-        .library(name: .coenttbServerTranslations, targets: [.coenttbServerTranslations]),
         .library(name: .coenttbDatabase, targets: [.coenttbDatabase]),
         .library(name: .coenttbServerUtils, targets: [.coenttbServerUtils]),
-        .library(name: .coenttbServerRouter, targets: [.coenttbServerRouter]),
+        .library(name: .coenttbServerRouter, targets: [.coenttbServerRouter])
     ],
     dependencies: [
-        .package(url: "https://github.com/coenttb/coenttb-utils.git", branch: "main"),
-        .package(url: "https://github.com/coenttb/coenttb-web.git", branch: "main"),
-        .package(url: "https://github.com/coenttb/pointfree-server.git", branch: "main"),
-        .package(url: "https://github.com/pointfreeco/swift-case-paths.git", from: "1.5.6"),
-        .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay.git", from: "1.4.3"),
+        .package(url: "https://github.com/coenttb/coenttb-utils", branch: "main"),
+        .package(url: "https://github.com/coenttb/swift-language", branch: "main"),
+        .package(url: "https://github.com/coenttb/swift-environment-variables", branch: "main"),
+        .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "1.5.6"),
+        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.9.2"),
+        .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.4.3"),
+        .package(url: "https://github.com/pointfreeco/swift-url-routing", from: "0.6.2"),
+        .package(url: "https://github.com/swift-server/async-http-client", from: "1.26.1"),
         .package(url: "https://github.com/vapor/postgres-kit", from: "2.12.0"),
+        .package(url: "https://github.com/apple/swift-log", from: "1.0.0")
     ],
     targets: [
         .target(
             name: .coenttbServer,
             dependencies: [
-                .coenttbWeb,
                 .coenttbServerEnvVars,
-                .coenttbServerHTML,
                 .coenttbServerDependencies,
-                .coenttbServerModels,
-                .coenttbServerTranslations,
                 .coenttbDatabase,
                 .coenttbServerUtils,
                 .coenttbServerRouter,
@@ -102,61 +86,80 @@ let package = Package(
         .target(
             name: .coenttbServerEnvVars,
             dependencies: [
-                .coenttbWebEnvVars,
-                .coenttbServerModels,
-            ]
-        ),
-        .target(
-            name: .coenttbServerHTML,
-            dependencies: [
-                .coenttbWebHTML,
-                .coenttbServerTranslations,
-                .coenttbServerDependencies,
+                .environmentVariables,
+                .logging
             ]
         ),
         .target(
             name: .coenttbServerDependencies,
             dependencies: [
-                .coenttbWeb,
-                .coenttbServerModels,
+                .asyncHttpClient,
+                .dependencies,
                 .postgresKit,
                 .issueReporting,
-                .pointfreeServer,
+                .logging
             ]
         ),
         .target(
             name: .coenttbServerUtils,
             dependencies: [
-                .coenttbWebUtils
-            ]
-        ),
-        .target(
-            name: .coenttbServerModels,
-            dependencies: [
-                .coenttbWebModels,
-            ]
-        ),
-        .target(
-            name: .coenttbServerTranslations,
-            dependencies: [
-                .coenttbWebTranslations
+                .logging
             ]
         ),
         .target(
             name: .coenttbDatabase,
             dependencies: [
-                .coenttbWeb
+                .languages
             ]
         ),
         .target(
             name: .coenttbServerRouter,
             dependencies: [
-                .coenttbServerTranslations,
                 .coenttbServerDependencies,
-                .coenttbServerModels,
                 .casePaths,
+                .dependencies,
+                .languages,
+                .urlRouting
             ]
         ),
+        .testTarget(
+            name: .coenttbServerDependencies.tests,
+            dependencies: [
+                .coenttbServerDependencies,
+                .dependenciesTestSupport
+            ]
+        ),
+        .testTarget(
+            name: .coenttbDatabase.tests,
+            dependencies: [
+                .coenttbDatabase,
+                .dependenciesTestSupport
+            ]
+        ),
+        .testTarget(
+            name: .coenttbServerEnvVars.tests,
+            dependencies: [
+                .coenttbServerEnvVars,
+                .dependenciesTestSupport
+            ]
+        ),
+        .testTarget(
+            name: .coenttbServerRouter.tests,
+            dependencies: [
+                .coenttbServerRouter,
+                .dependenciesTestSupport
+            ]
+        ),
+        .testTarget(
+            name: .coenttbServerUtils.tests,
+            dependencies: [
+                .coenttbServerUtils,
+                .dependenciesTestSupport
+            ]
+        )
+
     ],
     swiftLanguageModes: [.v6]
 )
+
+extension String { var tests: Self { self + " Tests" } }
